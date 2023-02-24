d723c95b3c9ffdf93f943b73bcd93379f8446d23
Bellamy <bdanbellamy@gmail.com>
2023-02-24 11:12:48 +0200

    Rendering header component

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {
-  createBrowserRouter, RouterProvider
+  createBrowserRouter, RouterProvider, Route, createRoutesFromElements
 } from 'react-router-dom';
 
 import App from './App'
+import Header from './components/header/header.component';
 import './index.css'
 import ShopPage from './pages/shop/shop.component';
 
 const router = createBrowserRouter([
-  {
-    exact: true,
-    path:"/",
-    element: <App/>,
-  },
-  {
-    exact: true,
-    path: "/shop",
-    element: <ShopPage/>,
-  }
+  createRoutesFromElements(
+    <Route path='/' element={<Header />}>
+      <Route path='' element={<App/>} />
+      <Route path="shop" element={<ShopPage />} />
+    </Route>
+  )
 ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
