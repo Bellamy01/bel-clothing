212d4377208e738d363722395ba31f71540f5a0d
Bellamy <bdanbellamy@gmail.com>
2023-03-04 09:55:43 +0200

    Creating user authentication pages

---
+++ src/pages/sign-up/sign-up-page.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './sign-up-page.styles.scss'
+import SignUp from '../../components/sign-in/sign-in.component';
+
+const SignUpPage = () => {
+    return(
+        <div className='sign-in'>
+            <SignIn />
+        </div>
+    )
+}
+
+export default SignUpPage;
