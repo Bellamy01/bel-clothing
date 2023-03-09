bcba1895220660ac09339b39e80a8093e16dadc3
Bellamy <bdanbellamy@gmail.com>
2023-03-09 21:30:10 +0200

    Adding user to firestore

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,6 +1,6 @@
 import  { initializeApp } from 'firebase/app';
 import { GoogleAuthProvider, getAuth, signInWithPopup } from 'firebase/auth';
-import { doc, collection } from 'firebase/firestore';
+import { addDoc, collection, getDocs, getFirestore, where, query, doc } from 'firebase/firestore';
 
 const config = {
     apiKey: "AIzaSyCkv07LhofqMv3inSjlzl8PLBBe5xh8cAk",
@@ -11,16 +11,37 @@ const config = {
     appId: "1:673727886285:web:e175bad16e673a39020a84",
     measurementId: "G-65WYCQL8FR"
 };
-//create user profile
+//initialize firebase
+const app = initializeApp(config);
+
+//initialize firestore
+const db = getFirestore(app);
+
 export const createUserProfileDocument = async (userAuth, additionalData) => {
     if(!userAuth) return;
 
-    const userRef = doc('users/f140914');
-    const snapShot = await userRef.get(); 
-    console.log();
-} 
+    const userQuery = query(collection(db,"users"),where("email","==",userAuth.email));
+    const snapShot = await getDocs(userQuery);
 
-const app = initializeApp(config);
+        if (snapShot.empty) {
+            const createdAt = new Date();
+            const { displayName, email } = userAuth;
+            try {
+                const docRef = await addDoc(doc(collection(db, "users")), {
+                    displayName,
+                    email,
+                    createdAt,
+                    ...additionalData
+                });
+                console.log("Document written with ID: ", docRef.id);
+                return docRef;
+            } catch (e) {
+                console.error("Error adding document: ", e);
+            }
+        
+    };
+    
+}
 
 //preferred language
 export const auth = getAuth();
@@ -34,6 +55,6 @@ provider.setCustomParameters({
 
 //sign in with popup
 export const signInWithGoogle = () => {
-    signInWithPopup(auth, provider)
+    signInWithPopup(auth, provider);
     }
 export default app;
