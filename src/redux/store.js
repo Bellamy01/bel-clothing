221632a446e0ae2225c9803fea5779e7b96b57ca
Bellamy <bdanbellamy@gmail.com>
2023-04-01 15:51:33 +0200

    Fixing non-serializable values errors

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,17 +1,9 @@
-import { /*applyMiddleware, */configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
-//import { legacy_createStore as createStore } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
 import logger from "redux-logger";
 
 import rootReducer from "./root-reducer";
 
-/* const middlewares = [logger];
-
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
-
-export default store; */
-
-
-const middlewares = [...getDefaultMiddleware(),logger];
+const middlewares = [logger];
 
 const store = configureStore({
     reducer: rootReducer,
