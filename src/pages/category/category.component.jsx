ad8ea6f5ef2ecacad1130bb8463bf1668c950770
Bellamy <bdanbellamy@gmail.com>
2023-04-04 22:31:10 +0200

    nested routing in shop page

---
+++ src/pages/category/category.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import CollectionItem from "../../components/collection-item/collection-item.component";
+
+import "./category.styles.scss"
+
+const CategoryPage = () => (
+    <div className="category">
+        dd
+    </div>
+)
+
+export default CategoryPage
